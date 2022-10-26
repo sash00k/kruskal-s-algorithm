324781f0aa433d41a7663eeeed47c0e8a1e88ab4
sash00k <s.a.c.h.a.b@yandex.ru>
2022-10-27 01:50:52 +0400

    4 lines of if-else into oneline construction

--- structures.py
+++ structures.py
@@ -3,10 +3,7 @@ class DisjointSet:
         self.parent = {elem: elem for elem in elems}
 
     def find_key(self, elem):
-        if self.parent[elem] == elem:
-            return elem
-        else:
-            return self.find_key(self.parent[elem])
+        return elem if self.parent[elem] == elem else self.find_key(self.parent[elem])
 
     def union(self, key1, key2):
         self.parent[key1] = key2
