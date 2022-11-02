98e5670cebc1f24645ab316c72ab87f3ee5e4fd9
sash00k <s.a.c.h.a.b@yandex.ru>
2022-11-03 02:09:39 +0400

    adding more fast solution

--- ethernet.py
+++ ethernet.py
@@ -1,8 +1,19 @@
-from structures import DisjointSet
+﻿class DisjointSet:
+    def __init__(self, elems):
+        self.parent = {elem: elem for elem in elems}
+        self.height_by_key = {elem: 1 for elem in elems}
+
+    def find_key(self, elem):
+        return elem if self.parent[elem] == elem else self.find_key(self.parent[elem])
+
+    def union_by_height(self, key1, key2):
+        if self.height_by_key[key2] < self.height_by_key[key1]:
+            key1, key2 = key2, key1
+
+        self.parent[key1] = key2
+        self.height_by_key[key2] = max(self.height_by_key[key2], self.height_by_key[key1] + 1)
 
 
-# solution based on disjoint-set
-# the solution has complexity O(N*log(N))
 def solution(data: str) -> (int, int):
     # read data
     rows = data.split('\n')
@@ -11,7 +22,7 @@ def solution(data: str) -> (int, int):
     # tree initializing
     forest = DisjointSet(list(range(tops_num)))
 
-    # list of lists [one_top, another_top, edge_weight] sorted by edge_weight ('if row' for last empty line in file)
+    # list of lists [fr, to, weight] sorted by edge_weight ('if row' for last empty line in file)
     edges_sorted = sorted([list(map(int, row.split())) for row in rows[1:] if row], key=lambda item: item[2])
 
     edges_used_num = 0
@@ -19,66 +30,14 @@ def solution(data: str) -> (int, int):
 
     # add edge with minimal weight to the forest if it doesn't produce cycle
     for edge in edges_sorted:
-        key1 = forest.find_key(elem=edge[0])
-        key2 = forest.find_key(elem=edge[1])
+        fr, to, weight = edge
+        key1 = forest.find_key(elem=fr)
+        key2 = forest.find_key(elem=to)
 
         if key1 != key2:
-            forest.union(key1, key2)
+            forest.union_by_height(key1, key2)
             edges_used_num += 1
-            sum_weight += edge[2]
-
-    # one tree has edges_num = tops_num - 1, so we may count trees in forest this way
-    trees_num = tops_num - edges_used_num
-
-    return trees_num, sum_weight
-	
-
-# solution based on python structure 'set' 
-# the solution has complexity O(N^2)
-﻿def solution_old(data: str) -> (int, int):
-    # read data
-    rows = data.split('\n')
-    tops_num, edges_num = map(int, rows[0].split())
-
-    # list of lists [one_top, another_top, edge_weight] sorted by edge_weight ('if row' for last empty line in file)
-    edges_sorted = sorted([list(map(int, row.split())) for row in rows[1:] if row], key=lambda item: item[2])
-
-    # list of sets, where each set includes tops from one tree
-    forest = []
-
-    edges_used_num = 0
-    sum_weight = 0
-
-    # add edge with minimal weight to the forest if it doesn't produce cycle
-    for edge in edges_sorted:
-        top_1 = edge[0]
-        top_2 = edge[1]
-
-        cycle_check_result = set()
-        for tree, tree_number in zip(forest, range(len(forest))):
-            # check whether this edge forms a cycle, if it does we get 'there_is_cycle'
-            if top_1 in tree and top_2 in tree:
-                cycle_check_result = 'there_is_cycle'
-                break
-
-            # if adding the edge doesn't form cycle, we return the indexes of trees to which this edge is attached
-            if top_1 in tree or top_2 in tree:
-                cycle_check_result.add(tree_number)
-
-        if cycle_check_result == 'there_is_cycle':
-            continue
-        else:
-            # forming updated_tree
-            updated_tree = {top_1, top_2}
-            for tree_num in cycle_check_result:
-                updated_tree |= forest[tree_num]
-
-            # drop trees, which are involved in forming updated_tree, and add updated_tree to forest
-            forest = [forest[tree_num] for tree_num in range(len(forest)) if tree_num not in cycle_check_result]
-            forest.append(updated_tree)
-
-        edges_used_num += 1
-        sum_weight += edge[2]
+            sum_weight += weight
 
     # one tree has edges_num = tops_num - 1, so we may count trees in forest this way
     trees_num = tops_num - edges_used_num
